dd4b41ba1e138a1a7930c73ad88cd31b707f2413
avish <avishusa45@gmail.com>
2024-07-16 11:21:41 -0500

    Implemented Navigation Routes and Basic Pages

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+
+      <h1>404 Not Found</h1>
+      <h3>The page you are looking for does not exist</h3>
+    </div>
+  )
+}
+
+export default NotFound
